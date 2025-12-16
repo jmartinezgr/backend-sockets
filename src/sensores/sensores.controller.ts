0a9aec3f5f30d03b9d2a9d0595bd7dba001ee183
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-12-16 01:37:48 -0500

    feat: Add Sensores module with controller and service
    Introduced the Sensores module, including its controller and service, to set up the basic structure for handling sensor-related functionality in the application.

---
+++ src/sensores/sensores.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('sensores')
+export class SensoresController {}
---
+++ src/sensores/sensores.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SensoresService } from './sensores.service';
+import { SensoresController } from './sensores.controller';
+
+@Module({
+  providers: [SensoresService],
+  controllers: [SensoresController],
+})
+export class SensoresModule {}
