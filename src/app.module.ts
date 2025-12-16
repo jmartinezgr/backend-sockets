e58e57f67e2d62deb2017172dfe4db488bc725f7
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-12-16 01:35:16 -0500

    feat: Add usuarios module with controller and service
    Introduced a new 'usuarios' module including its controller and service. Updated app.module.ts to import the new module. Added dependencies for TypeORM, PostgreSQL, and configuration management in package.json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebSocketModule } from './websockets/websocket.module';
+import { SensoresModule } from './sensores/sensores.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [WebSocketModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        synchronize:
+          config.get<string>('ENVIRONMENT') === 'development' ? true : false, // solo en dev
+      }),
+    }),
+    WebSocketModule,
+    SensoresModule,
+    UsuariosModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
